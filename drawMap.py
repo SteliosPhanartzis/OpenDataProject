c5cf7b284b9c85f423b1ed9724183d27b92aedc3
boomvausstat <steliftw@gmail.com>
2018-02-15 20:32:35 -0500

    Update drawMap.py

--- drawMap.py
+++ drawMap.py
@@ -1,1 +1,9 @@
-import geopy
+#import geopy
+#pip install --upgrade folium
+#pip install --upgrade pandas
+
+folium_map = folium.Map(location=[40.738, -73.98],
+                        zoom_start=13,
+                        tiles="CartoDB dark_matter")
+marker = folium.CircleMarker(location=[40.738, -73.98])
+marker.add_to(folium_map)
